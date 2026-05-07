eeb2169ee508f5c6595bd60c01b14258b710eff3
ohyoujin <hynan12@gmail.com>
2026-05-07 14:59:39 +0900

    redux save

---
+++ redux-test/src/CounterA.tsx
@@ -0,0 +1,13 @@
+import { useDispatch } from "react-redux"
+import { increase } from "./actions"
+
+export default function CounterA(){
+    const dispatch = useDispatch()
+    return(
+        <div>
+            <button onClick= {() => dispatch(increase())}>
+                <h1>+</h1>
+            </button>
+        </div>
+    )
+}
---
+++ redux-test/src/CounterB.tsx
@@ -0,0 +1,13 @@
+import { useDispatch } from "react-redux"
+import { decrease } from "./actions"
+
+export default function CounterB(){
+    const dispatch = useDispatch()
+    return(
+        <div>
+            <button onClick= {() => dispatch(decrease())}>
+                <h1>-</h1>
+            </button>
+        </div>
+    )
+}
